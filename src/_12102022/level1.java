42a207d41d83bd9a3089d4251715c8055829dc9f
Alexey Lavrov <alexey.l@diffco.us>
2022-10-13 13:35:32 +0200

    first level

---
+++ src/_12102022/level1.java
@@ -0,0 +1,12 @@
+package _12102022;
+
+public class level1 {
+    public static void main(String[] args) {
+        int num1, num2, num3, sum;
+        num1 = 33;
+        num2 = 20;
+        num3 = 55;
+        sum = num1 + num2 + num3;
+        System.out.println("sum=" + sum);
+    }
+}
